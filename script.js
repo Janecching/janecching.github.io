c47275d6d1db69059de32190ca8f97e7aaf44f81
jane__ <35574188+Janecching@users.noreply.github.com>
2021-11-05 22:44:14 -0700

    popup

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ $(document).ready(function(){
         nextArrow:"<img class='a-right control-c next slick-next' src='right.jpg'>"
     });
 });
+
+
+
+function popupOpenClose(popup) {
+
+  /* Add div inside popup for layout if one doesn't exist */
+  if ($(".wrapper", popup).length == 0) {
+    $(popup).wrapInner("<div class='wrapper'></div>");
+  }
+
+  /* Open popup */
+  $(popup).show();
+
+  /* Close popup if user clicks on background */
+  $(popup).click(function(e) {
+    if (e.target == this) {
+      if ($(popup).is(':visible')) {
+        $(popup).hide();
+      }
+    }
+  });
+
+  /* Close popup and remove errors if user clicks on cancel or close buttons */
+  $(popup).find(".popup-btn-close").on("click", function() {
+    if ($(".formElementError").is(':visible')) {
+      $(".formElementError").remove();
+    }
+    $(popup).hide();
+  });
+}
+
+$(document).ready(function() {
+  $(".popup-trigger").on("click", function() {
+    var target = $(this).data('target');
+    popupOpenClose($(target));
+  });
+});
